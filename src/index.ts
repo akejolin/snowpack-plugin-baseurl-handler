e5f252672cff2d39dfa1a5e82ed23ea6721c560f
akejolin <akejolin@gmail.com>
2020-08-20 00:03:35 +0200

    Edited plugin script. Also made build command work

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,25 @@
 import type { SnowpackPluginFactory } from "snowpack";
-import pkg from '../package.json'
-const plugin: SnowpackPluginFactory = () => ({
-  name: pkg.name,
-  input: [".js"],
-  async build({ contents, fileExt, filePath, isDev }: any) {
-    console.table({ contents, fileExt, filePath, isDev });
-    return contents;
-  },
-});
 
+const plugin: SnowpackPluginFactory = () => {
+
+  let isDev:boolean = true;
+
+  return {
+    name: 'snowpack-plugin-baseurl-handler',
+    input: [".js"],
+    async run(options:any) {
+      isDev = !!options.isDev;
+    },
+    async transform({fileExt, contents}:any) {
+
+      let _baseurl = !isDev && process.env.BASE_URL && process.env.BASE_URL !== '/' ? `/${process.env.BASE_URL}` : ''
+
+      const testFileExt:Array<string>= ['.html', '.js', '.jsx', '.css', '.txt']
+
+      if (testFileExt.find(fileExt)) {
+        return contents.replace(/%BASE_URL%/gi, `${_baseurl}`)
+      }
+    },
+  };
+}
 export default plugin;
