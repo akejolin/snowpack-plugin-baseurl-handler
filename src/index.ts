9a25157dcbf03f9656fb8d113c411bb3b4611a43
akejolin <akejolin@gmail.com>
2020-08-20 22:52:21 +0200

    added typescripts

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,30 @@
-import type { SnowpackPluginFactory } from "snowpack";
 
-const plugin: SnowpackPluginFactory = () => {
 
+const plugin:object = (snowpackConfig:object, pluginOptions:object) => {
+  const { appBaseUrl}:any = pluginOptions;
+  const { baseUrl }:any = snowpackConfig
   let isDev:boolean = true;
 
   return {
     name: 'snowpack-plugin-baseurl-handler',
-    input: [".js"],
     async run(options:any) {
       isDev = !!options.isDev;
     },
+    
     async transform({fileExt, contents}:any) {
-
-      let _baseurl = !isDev && process.env.BASE_URL && process.env.BASE_URL !== '/' ? `/${process.env.BASE_URL}` : ''
+      if (baseUrl) {
+        return contents.replace(/%BASE_URL%/gi, ``)
+      }
+      let _appBaseurl:string = !isDev && appBaseUrl && appBaseUrl !== '/' ? `/${appBaseUrl}` : ''
 
       const testFileExt:Array<string>= ['.html', '.js', '.jsx', '.css', '.txt']
 
       if (testFileExt.find(fileExt)) {
-        return contents.replace(/%BASE_URL%/gi, `${_baseurl}`)
+        return contents.replace(/%BASE_URL%/gi, `${_appBaseurl  }`)
       }
+      //return null
     },
+    
   };
 }
 export default plugin;
